3763fa5bcc66947a0a9a2db3f042b46bf637e261
saulukas <saulius.narkevicius@tieto.com>
2016-07-30 17:15:34 +0300

    updated for year 2016

--- src/main/java/lt/tieto/msi2016/item/service/ItemService.java
+++ src/main/java/lt/tieto/msi2016/item/service/ItemService.java
@@ -4,18 +4,19 @@ package lt.tieto.msi2016.item.service;
 import lt.tieto.msi2016.item.model.Item;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ItemService {
 
-    private static final List<Item> ITEMS_LIST = Arrays.asList(
+    private static final List<Item> ITEMS_LIST = new ArrayList<>(Arrays.asList(
             new Item(1l, "Item 1", 1, "20x20"),
             new Item(2l, "Item 2", 10, "200x120"),
             new Item(3l, "Item 3", 15, "20x10"),
             new Item(4l, "Item 4", 15, "20x10")
-    );
+    ));
 
     public Item get(Long id) {
         return getItemById(id);
